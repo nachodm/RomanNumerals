fec2337196ae0aa9d6d6022f9e56a80305aeb229
idomingo <idomingo@STUPID-PC409.stnet.local>
2016-10-05 16:43:34 +0300

    Finished task

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,72 @@
-
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		int number = 0;
 		
+		int[] array;
+ 		array = new int[romanNum.length()];
+		
+		for (int i = 0; i < romanNum.length(); i++) {
+			String aux = romanNum.substring(i, i + 1);
+			if (aux.equals("I")) {
+				array[i] = 1;
+			}
+			else if (aux.equals("V")) {
+				array[i] = 5;
+			}
+			else if (aux.equals("X")) {
+				array[i] = 10;
+			}
+			else if (aux.equals("L")) {
+				array[i] = 50;
+			}
+			else if (aux.equals("C")) {
+				array[i] = 100;
+			}
+			else if (aux.equals("D")) {
+				array[i] = 500;
+			}
+			else if (aux.equals("M")) {
+				array[i] = 1000;
+			}
+		}
+		int counter = 0;
+		for (int i = 0; i < romanNum.length(); i++) {
+			if (counter <= 0) {
+				if ((i == romanNum.length() - 1) || (array[i] > array[i + 1])) {
+					number = number + array[i];
+				}
+				else if (array[i] == array [i + 1]){
+					if (i + 1 == romanNum.length() - 1) {
+						number = number + array[i] * 2;
+						counter = 3;
+					}
+					else if (array[i + 1] == array [i + 2]) {
+						if (i + 2 == romanNum.length() - 1) {
+							number = number + array[i] * 3;
+							counter = 3;
+						}
+						else {
+							number = number + array[i + 3] - array[i] * 3;
+							counter = 4;
+						}
+					}
+					else if (array[i+2] < array[i+1]) {
+						number = number + array[i] * 2;
+						counter = 2;
+					}
+					else{
+						number = number + array[i + 2] - array[i] * 2;
+						counter = 3;
+					}
+				}
+				else {
+					number = number + array[i + 1] - array[i];
+					counter = 2;
+				}
+			}
+			counter--;
+		}
+		return number;
 	}
 }
+	
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,64 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
+	
+	RomanNumerals romanNum = new RomanNumerals();
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test public void testI_1() {
+		String I = "I";
+		assertEquals(romanNum.convertToInteger(I), 1);
+	}
+	
+	@Test public void testV_5() {
+		String V = "V";
+		assertEquals(romanNum.convertToInteger(V), 5);
 	}
 
+	@Test public void testX_10() {
+		String X = "X";
+		assertEquals(romanNum.convertToInteger(X), 10);
+	}
+	@Test public void testL_50() {
+		String L = "L";
+		assertEquals(romanNum.convertToInteger(L), 50);
+	}
+	
+	@Test public void testC_100() {
+		String C = "C";
+		assertEquals(romanNum.convertToInteger(C), 100);
+	}
+
+	@Test public void testD_500() {
+		String D = "D";
+		assertEquals(romanNum.convertToInteger(D), 500);
+	}
+	
+	@Test public void testM_1000() {
+		String M = "M";
+		assertEquals(romanNum.convertToInteger(M), 1000);
+	}
+	@Test public void testII_2() {
+		String I = "II";
+		assertEquals(romanNum.convertToInteger(I), 2);
+	}
+	
+	@Test public void testCCC_300() {
+		String CCC = "CCC";
+		assertEquals(romanNum.convertToInteger(CCC), 300);
+	}
+	
+	@Test public void testMMXVI_2016() {
+		String  MMXVI = "MMXVI";
+		assertEquals(romanNum.convertToInteger(MMXVI), 2016);
+	}
+	
+	@Test public void testMMXIV_2014() {
+		String  MMXIV = "MMXIV";
+		assertEquals(romanNum.convertToInteger(MMXIV), 2014);
+	}
+	
+	@Test public void testMCMXCIV_1994() {
+		String MCMXCIV = "MCMXCIV";
+		assertEquals(romanNum.convertToInteger(MCMXCIV), 1994);
+	}
 }
